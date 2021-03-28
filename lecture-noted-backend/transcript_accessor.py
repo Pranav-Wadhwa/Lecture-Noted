f9321d76d485a7e895af810a1beb9c59f7bc292a
vbhaip <vbhaip01@gmail.com>
2021-03-28 11:06:59 -0400

    idk why its messing up

--- lecture-noted-backend/transcript_accessor.py
+++ lecture-noted-backend/transcript_accessor.py
@@ -35,6 +35,7 @@ def get_mp3_transcript(url, path):
 
 # returns [(chunk, start_time)]
 def get_chunky_transcript(video_id):
+    raw = YouTubeTranscriptApi.get_transcript(video_id)
     try:
         raw = YouTubeTranscriptApi.get_transcript(video_id)
     except:
