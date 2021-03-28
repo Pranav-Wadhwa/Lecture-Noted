ff05836cf7a9d73dfdbd38054e61c98602dba5ab
vbhaip <vbhaip01@gmail.com>
2021-03-28 09:51:36 -0400

    timestamp + testing endpoint

--- lecture-noted-backend/app.py
+++ lecture-noted-backend/app.py
@@ -60,9 +60,9 @@ def mp3notes():
 
 @app.route('/notes/<string:vid>')
 def notes(vid):
-    # transcript = trans.get_transcript(vid)
+    transcript = trans.get_transcript(vid)
     # chunks = trans.chunk(transcript)
-    chunks, times = get_chunky_transcript(vid)
+    chunks, times = trans.get_chunky_transcript(vid)
 
     data = []
     for i in range(len(chunks)):
@@ -83,6 +83,9 @@ def notes(vid):
     # for i in range(0, len(data)):
     #     data[i] = {"type": "text", "data": data[i]}
 
+    #print(data)
+    #print(transcript)
+
     data = data + [{"type": "image", "data": item} for item in kf.get_images(transcript)]
 
     metadata = trans.get_metadata(vid)
@@ -98,6 +101,14 @@ def testing():
 
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+@app.route('/testingv2')
+def testing2():
+    response= jsonify({ "metadata": { "author": "minutephysics", "dislikes": 601, "duration": "00:01:25", "likes": 18193, "rating": 4.872087, "title": "Minute Physics: What is Gravity?", "viewcount": 2809342 }, "response": [ { "data": "Every object attracts every other object in the universe. The strength of the attraction decreases by the square of the distance between the objects.", "time": "2.95", "type": "text" }, { "data": "The attraction is proportional to the mass of the objects.", "type": "text" }, { "data": "Objects with mass also attract massless particles like light.", "type": "text" }, { "data": { "image": "https://pixabay.com/get/g3bea1aca3179c3747c2468afc8075e72d472a723b90b372e491078f344e88e274eef89e8fad5ea4fe9656d5c93cc55d6526d7037e09b6b2a8df6b8d152fd1835_1280.jpg", "keyword": "objects" }, "type": "image" }, { "data": { "image": "https://pixabay.com/get/gfbe95589d133a725afc3fef40c17bfda673c38a61e320344e96602b5ed6e02c2b8770356eb3ec066025093fc526bfb2b17285e58b79e0334786ce107adf06b62_1280.jpg", "keyword": "attraction" }, "type": "image" }, { "data": { "image": "https://pixabay.com/get/g1660fb43734a0074ea79a2b04cd17dae4eb0d4d7f00861c32960d2875425998ed9fcd5e429d871918ec29a0a116d055723614f4ed3a73ff297d455abc93f7f5e_1280.jpg", "keyword": "light" }, "type": "image" } ] })
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 
 @app.route('/docx')
 def generate_doc():
