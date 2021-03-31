8da89543c186649b2a2d50c4f412ad659ce4e3cf
vbhaip <vbhaip01@gmail.com>
2021-03-30 23:03:41 -0400

    comment out endpoints that ping openai

--- lecture-noted-backend/app.py
+++ lecture-noted-backend/app.py
@@ -29,93 +29,94 @@ def transcript(vid):
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@app.route('/mp3notes/')
-def mp3notes():
-    rawurl = request.query_string.decode().split("?")[0].split("=")[1]
-    extension = request.query_string.decode().split("?")[-1]
-    #print(rawurl)
-    loc = urllib.parse.unquote_plus(rawurl)
-    #print("we made it")
-    #print(loc)
-    #print(loc.split("/")[-1].split("?")[0])
-
-    transcript = trans.get_mp3_transcript(rawurl+"?"+extension, loc.split("/")[-1].split("?")[0] + ".wav")
-    chunks = trans.chunk(transcript)
-    #print("we vibin")
-    #print(chunks)
-
-    data = []
-    for chunk in chunks:
-        data = data + vid_sum.summarize(chunk)
-
-
-    for i in range(0, len(data)):
-        data[i] = {"type": "text", "data": data[i]}
-
-    data = data + [{"type": "image", "data": item} for item in kf.get_images(transcript)]
-
-    response = jsonify({"response": data, "metadata": {"filename": rawurl+"?"+extension}})
-
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-
-@app.route('/notes/<string:vid>')
-def notes(vid):
-    
-    # chunks = trans.chunk(transcript)
-    chunks, times = trans.get_chunky_transcript(vid)
-    #print(chunks)
-
-    if(chunks is None):
-        response = jsonify({"response": "no transcript"})
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
-
-    transcript = ''.join(chunks)
-
-    #print("done chunking")
-
-    data = []
-    for i in range(len(chunks)):
-        if((float)(times[i])/60 > MAX_PROCESS_TIME):
-            break
-        bullets = vid_sum.summarize(chunks[i])
-
-        #print(bullets)
-        #print(times)
-        if(bullets is None):
-            continue
-
-        #yield " "
-
-        #    idk if we want to do this
-        # if len(bullets) == 1:
-        #     bullets = vid_sum.summarize(chunks[i])
-
-        data.append({'type': 'text', 'data': bullets[0], 'time': times[i]})
-        for bullet in bullets[1:]:
-            data.append({'type': 'text', 'data': bullet})
-
-    # data = []
-    # for chunk in chunks:
-    #     data = data + vid_sum.summarize(chunk)
-
-    # for i in range(0, len(data)):
-    #     data[i] = {"type": "text", "data": data[i]}
-
-    #print(data)
-    #print(transcript)
-
-    data = data + [{"type": "image", "data": item} for item in kf.get_images(transcript)]
-
-    metadata = trans.get_metadata(vid)
-
-    response = jsonify({"response": data, "metadata": metadata})
-
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
+#commented out so it doesn't ping open ai endpoint
+#@app.route('/mp3notes/')
+#def mp3notes():
+#    rawurl = request.query_string.decode().split("?")[0].split("=")[1]
+#    extension = request.query_string.decode().split("?")[-1]
+#    #print(rawurl)
+#    loc = urllib.parse.unquote_plus(rawurl)
+#    #print("we made it")
+#    #print(loc)
+#    #print(loc.split("/")[-1].split("?")[0])
+#
+#    transcript = trans.get_mp3_transcript(rawurl+"?"+extension, loc.split("/")[-1].split("?")[0] + ".wav")
+#    chunks = trans.chunk(transcript)
+#    #print("we vibin")
+#    #print(chunks)
+#
+#    data = []
+#    for chunk in chunks:
+#        data = data + vid_sum.summarize(chunk)
+#
+#
+#    for i in range(0, len(data)):
+#        data[i] = {"type": "text", "data": data[i]}
+#
+#    data = data + [{"type": "image", "data": item} for item in kf.get_images(transcript)]
+#
+#    response = jsonify({"response": data, "metadata": {"filename": rawurl+"?"+extension}})
+#
+#    response.headers.add('Access-Control-Allow-Origin', '*')
+#    return response
+#
+#commented out so no more open ai endpoint requests go through
+#@app.route('/notes/<string:vid>')
+#def notes(vid):
+#    
+#    # chunks = trans.chunk(transcript)
+#    chunks, times = trans.get_chunky_transcript(vid)
+#    #print(chunks)
+#
+#    if(chunks is None):
+#        response = jsonify({"response": "no transcript"})
+#        response.headers.add('Access-Control-Allow-Origin', '*')
+#        return response
+#
+#    transcript = ''.join(chunks)
+#
+#    #print("done chunking")
+#
+#    data = []
+#    for i in range(len(chunks)):
+#        if((float)(times[i])/60 > MAX_PROCESS_TIME):
+#            break
+#        bullets = vid_sum.summarize(chunks[i])
+#
+#        #print(bullets)
+#        #print(times)
+#        if(bullets is None):
+#            continue
+#
+#        #yield " "
+#
+#        #    idk if we want to do this
+#        # if len(bullets) == 1:
+#        #     bullets = vid_sum.summarize(chunks[i])
+#
+#        data.append({'type': 'text', 'data': bullets[0], 'time': times[i]})
+#        for bullet in bullets[1:]:
+#            data.append({'type': 'text', 'data': bullet})
+#
+#    # data = []
+#    # for chunk in chunks:
+#    #     data = data + vid_sum.summarize(chunk)
+#
+#    # for i in range(0, len(data)):
+#    #     data[i] = {"type": "text", "data": data[i]}
+#
+#    #print(data)
+#    #print(transcript)
+#
+#    data = data + [{"type": "image", "data": item} for item in kf.get_images(transcript)]
+#
+#    metadata = trans.get_metadata(vid)
+#
+#    response = jsonify({"response": data, "metadata": metadata})
+#
+#    response.headers.add('Access-Control-Allow-Origin', '*')
+#    return response
+#
 @app.route('/testing')
 def testing():
     response = jsonify({"metadata":{"author":"minutephysics","dislikes":601,"duration":"00:01:25","likes":18193,"rating":4.872087,"title":"Minute Physics: What is Gravity?","viewcount":2809263},"response":[{"data":"Gravity is a long range attractive force between all objects with mass.","type":"text"},{"data":"Every massive object attracts every other in the universe.","type":"text"},{"data":"The strength of gravity decreases by the square of the distance between two objects \u2013 so if you're twice as far away, gravity is only one fourth as strong!","type":"text"}]})
